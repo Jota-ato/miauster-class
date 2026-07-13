6de077391e8e035be9e9fde6359c9b8b67c11154
Julio César Zavala Blanco <julioc-01@outlook.com>
2026-07-13 08:41:28 -0600

    remove database url

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,7 +3,7 @@ import { drizzle } from 'drizzle-orm/neon-http'
 import * as schema from './schema'
 
 export const db = drizzle(
-	"postgresql://neondb_owner:npg_4a8qosvyDfJR@ep-ancient-king-ai5xb1oc-pooler.c-4.us-east-1.aws.neon.tech/neondb?channel_binding=require&sslmode=require", {
+	process.env.DATABASE_URL!, {
 		schema
 	}
 )
